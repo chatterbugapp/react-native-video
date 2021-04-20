807e2fc02673a62f266db1c4135328300124cc62
Anaïs Betts <anais@anaisbetts.org>
2021-04-20 13:53:07 +0200

    Florp all of our package paths

--- examples/basic/metro.config.js
+++ examples/basic/metro.config.js
@@ -37,11 +37,11 @@ module.exports = {
       ),
       // Prevent recursive node_modules from local react-native-video
       new RegExp(
-        `${path.resolve(__dirname, 'node_modules/react-native-video/node_modules').replace(/[/\\]/g, '/')}.*`,
+        `${path.resolve(__dirname, 'node_modules/@chatterbug/react-native-video/node_modules').replace(/[/\\]/g, '/')}.*`,
       ),
       // Prevent recursive examples from local react-native-video
       new RegExp(
-        `${path.resolve(__dirname, 'node_modules/react-native-video/examples').replace(/[/\\]/g, '/')}.*`,
+        `${path.resolve(__dirname, 'node_modules/@chatterbug/react-native-video/examples').replace(/[/\\]/g, '/')}.*`,
       ),
     ]),
   },
